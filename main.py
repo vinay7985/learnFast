ee6716b9e0d54b43198230736ba11b569c01bcd5
rajeshsharma65 <sharmarajesh956542@gmail.com>
2023-09-05 19:31:14 +0530

    BAC-22 updated login

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-from typing import Dict, List, Union
+from typing import Dict, List, Union, Annotated
 from fastapi import FastAPI, Request, Depends, HTTPException, status, Response
 from database import engine,SessionLocal
 import models,schemas
-from models import Items
-from schemas import  Items
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from schemas import  Login
 from sqlalchemy.orm import Session
 import services as _services
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-templates = Jinja2Templates(directory="templates")
+import jwt
+
+
 
 app = FastAPI()
 def get_db():
@@ -19,16 +21,49 @@ def get_db():
     finally:
         db.close()   
 
+_JWT_SECRET = 'JWT_SECRET'
+
 
 models.Base.metadata.create_all(engine)
 
+def fake_hash_password(password: str):
+    return "fakehashed" + password
 
 
+templates = Jinja2Templates(directory="templates")
+@app.get("/login", response_class=HTMLResponse )
+async def login(request: Request):
+    return templates.TemplateResponse('index.html', {'request': request})
 
-@app.get("/index", response_class=HTMLResponse)
-async def index(request: Request):
-    return templates.TemplateResponse("index.html",  {"request": request})
-
+@app.post("/login"  )
+async def login( request: Request, form_data:OAuth2PasswordRequestForm=Depends(), db: Session = Depends(get_db)):
+    # form=await request.form()
+    # email= form.get("email")
+    # password= form.get("password")
+    errors=[]
+    # try:
+    # user=db.query(models.User).filter(models.User.email == email).first()
+    user=_services.get_user_by_email(db=db, email=form_data.email)
+    if user is None:
+           errors.append('Email is not exist')
+           return templates.TemplateResponse('index.html', {'request': request, 'errors':errors})
+    else:
+             if _services.varify_password(form_data.password, user.hashed_password):
+                 token=_services.create_token(user)
+                 return {"access_token":token, "token_type":"bearer"}
+            #  if hash.varify_password(password, user.password):
+            #      data={"sub":email}
+            #      return data
+                #  jwt_token= jwt.encode(data, _JWT_SECRET , algorithm='HS256')
+                #  Response.set_cookie(key="access_token", value=f"Bearer {jwt_token}", httponly=True)
+                #  msg="Login successfully"
+                #  return templates.TemplateResponse('index.html', {'request': request, 'msg':msg})
+            #   else:
+            #      errors.append("Invalid Password")
+            #      return templates.TemplateResponse('index.html', {'request': request, 'errors':errors})
+    #  except:
+    #     errors.append('something went wrong')
+    #     return templates.TemplateResponse('index.html', {'request': request, 'errors':errors})
 
 
 @app.get("/items")
--- schemas.py
+++ schemas.py
@@ -2,6 +2,11 @@ from pydantic import BaseModel
 from typing import Optional
 import datetime as _dt
 
+class Login(BaseModel):
+    email: str
+    password:str
+    
+
 class Items(BaseModel):
     name: str
     description:str
